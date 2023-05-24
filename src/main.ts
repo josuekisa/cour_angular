67458bb0d759db4ea9ea1171f50b307826ef4aad
josuekisa <josue.kisa31@gmail.com>
2023-05-24 11:28:27 +0200

    exo 1 fini

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,55 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Component, NgModule, platformCore } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: 'app-header',
+  template: '<h3>Hey salut</h3>'
+})
+class HeaderComponent {}
+
+@Component({
+  selector: 'app-section',
+  template :  `<p>Bienvenue dans mon site jeune chevalier</p> `
+})
+class AppSection{}
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+@Component({
+  selector: 'app-footer',
+  template: `
+    <footer>
+      &copy; josue - {{ year }}
+    </footer>
+  `
+})
+class FooterComponent {
+  year = '2023';
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <app-header></app-header>
+      <app-section></app-section>
+      <app-footer></app-footer>
+    </div>
+  `
+})
+
+
+
+class AppComponent {}
+
+
+
+@NgModule({
+  declarations: [AppComponent,HeaderComponent,AppSection,FooterComponent,],
+  imports: [ BrowserModule ],
+  bootstrap:[AppComponent]
+})
+class AppModule{}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
